11b230da55d11c070eeebf53d3ebbc1987714e21
Tomnl <thomas.nigel.lawson@gmail.com>
2018-08-27 14:31:51 +0100

    requirements update

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'django==1.11',
+        'django==1.11.*',
         'django-galaxy',
         'django-misa',
     ],
