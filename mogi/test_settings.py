d68077e15f5efb360073e69a1bb5f06b4341f66a
Thomas N Lawson <thomas.nigel.lawson@gmail.com>
2024-02-21 14:42:06 +0000

    Update test settings for allauth

--- mogi/test_settings.py
+++ mogi/test_settings.py
@@ -45,6 +45,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    "allauth.account.middleware.AccountMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
