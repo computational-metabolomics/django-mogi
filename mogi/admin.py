cccac48798596c8ce67a950e3beb113e24e6526b
Thomas N Lawson <thomas.nigel.lawson@gmail.com>
2026-02-18 14:29:06 +0000

    add more compounds list to admin (#21)

--- mogi/admin.py
+++ mogi/admin.py
@@ -1,8 +1,17 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.contrib import admin
 from django.apps import apps
+from mogi.models.models_compounds import Compound
+
+
+class CompoundAdmin(admin.ModelAdmin):
+    list_per_page = 1000
+
 
 mogi = apps.get_app_config('mogi')
-admin.site.register(mogi.get_models())
+admin.site.register(Compound, CompoundAdmin)
+
+for model in mogi.get_models():
+    if model is Compound:
+        continue
+    admin.site.register(model)
